b18e1c20d8e47d30f5c31397f1adb77845e7e1dc
Ronit Kedia <kediaronit.21@gmail.com>
2025-09-29 02:40:07 +0530

    refactor(dashboard): update user session handling and improve onboarding logic

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,27 +1,24 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
-import Navbar from "./_components/Navbar";
+import { getServerUserSession } from "../utils/getServerUserSession";
+import Navbar from "./_components/navbar/Navbar";
 import NewUserOnboarding from "./_components/NewUserOnboarding";
 import ProjectsSection from "./_components/ProjectsSection";
 
 export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
+  const user = await getServerUserSession();
 
-  if (!session) {
+  if (!user) {
     redirect("/auth");
   }
 
-  const user = await prisma.user.findUnique({
-    where: { id: session.user.id },
+  const userExist = await prisma.user.findUnique({
+    where: { id: user.id },
     select: { isOnboarded: true },
   });
 
-  const showOnboarding = !user?.isOnboarded;
+  const showOnboarding = !userExist?.isOnboarded;
 
   return (
     <div className="bg-sidebar min-h-lvh">

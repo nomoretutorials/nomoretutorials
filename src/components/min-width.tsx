de1c6bf13d5b13fda89799a7be791f4aba03ca7c
Ronit Kedia <kediaronit.21@gmail.com>
2025-09-29 17:30:30 +0530

    feat(MinWidth): add sign-out button and improve layout for small screens

--- src/components/min-width.tsx
+++ src/components/min-width.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { ReactNode, useEffect, useState } from "react";
-import { FullscreenIcon } from "lucide-react";
+import { FullscreenIcon, LogOutIcon } from "lucide-react";
+import { toast } from "sonner";
+
+import { authClient } from "@/lib/auth-client";
+import { Button } from "./ui/button";
 
 const EXCLUDED_PATHS = ["/auth", "/privacy-policy", "/terms"];
 
 export default function MinWidth({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const [isTooSmall, setIsTooSmall] = useState(false);
+  const router = useRouter();
 
   const isExcluded = EXCLUDED_PATHS.includes(pathname);
 
@@ -22,20 +27,42 @@ export default function MinWidth({ children }: { children: ReactNode }) {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  const handleSignOut = async () => {
+    try {
+      await authClient.signOut({
+        fetchOptions: {
+          onSuccess: () => {
+            router.push("/auth"); // redirect to login page
+          },
+        },
+      });
+    } catch {
+      toast.error("Error Signing Out");
+    }
+  };
+
   if (isExcluded) return <>{children}</>;
 
   if (isTooSmall) {
     return (
-      <div className=" max-w-lg mx-auto bg-background flex h-screen w-screen items-center justify-center px-6 text-white">
+      <div className="bg-background relative mx-auto flex h-screen w-screen max-w-lg items-center justify-center px-6 text-white">
+        <Button
+          className="absolute top-6 right-6 rounded-2xl"
+          variant={"outline"}
+          onClick={handleSignOut}
+        >
+          <LogOutIcon />
+          <span>Sign Out</span>
+        </Button>
         <div className="flex flex-col items-start justify-start space-y-10">
           <div className="bg-sidebar rounded-2xl p-4">
             <FullscreenIcon size={"40"} />
           </div>
           <div className="space-y-2.5">
-            <h1 className="text-2xl md:text-3xl font-bold">Small screens not supported (yet)</h1>
-            <p className="text-gray-400 ">
-              We’re still working on making it mobile-friendly.
-              For now, please open this on a laptop or PC. 🚀
+            <h1 className="text-2xl font-bold md:text-3xl">Small screens not supported (yet)</h1>
+            <p className="text-gray-400">
+              We’re still working on making it mobile-friendly. For now, please open this on a
+              laptop or PC. 🚀
             </p>
           </div>
         </div>

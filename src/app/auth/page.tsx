65e6fb5246af1e4f3865eb047c8cad5d68e604a9
Ronit Kedia <kediaronit.21@gmail.com>
2025-09-29 17:38:31 +0530

    fix(auth): adjust layout of terms and conditions text for better alignment on small screens

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -22,7 +22,7 @@ export default async function AuthPage() {
     <div className="flex h-screen w-full max-w-4xl items-center justify-between gap-12 overflow-y-auto p-4 sm:p-6 md:h-full lg:w-4xl lg:p-8">
       <div className="flex h-full flex-1 flex-col items-center justify-center gap-8">
         <LoginForm />
-        <div className="text-muted-foreground *:[a]:hover:text-primary w-full text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        <div className="max-w-sm mx-auto text-muted-foreground *:[a]:hover:text-primary w-full text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
           By clicking continue, you agree to our <a href="/terms">Terms of Service</a> and{" "}
           <a href="/privacy-policy">Privacy Policy</a>.
         </div>

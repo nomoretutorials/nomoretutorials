9a89ac0adeb97c0b83f694834c182fd398e85d52
Ronit Kedia <kediaronit.21@gmail.com>
2025-10-01 22:17:32 +0530

    refactor: update params type to Promise and adjust project retrieval logic

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -4,13 +4,14 @@ import { getProject } from "@/actions/projectActions";
 import SpecificProjectPageClient from "../_components/SpecificProjectPageClient";
 
 type Props = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 // TODO: Implement try/catch
 
 export default async function SpecificProjectPage({ params }: Props) {
-  const { project } = await getProject(params.id);
+  const { id } = await params;
+  const { project } = await getProject(id);
 
   if (!project) {
     notFound();

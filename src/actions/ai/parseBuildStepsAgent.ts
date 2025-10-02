6b316f2f2232e6f26d105ec2fcd0a5ff489ccdd0
Ronit Kedia <kediaronit.21@gmail.com>
2025-10-02 15:37:07 +0530

    feat(ParseBuildStepsAgent): implement function to parse build steps using agent and validation schema

---
+++ src/actions/ai/parseBuildStepsAgent.ts
@@ -0,0 +1,23 @@
+import { buildStepsAgent } from "@/inngest/agents/buildStepsAgent";
+import { BuildStepListSchema, BuildStepsList } from "@/schemas/agentResponseValidation";
+import { runAndValidateAgent } from "@/utils/runAndValidateAgent";
+
+export async function parseBuildStepsAgent(
+  title: string,
+  description: string,
+  techStackNames: string,
+  featuresList: string
+) {
+  const parsed = await runAndValidateAgent<BuildStepsList>(
+    buildStepsAgent,
+    `Generate step titles for building this project:
+        Title: ${title},
+        Description: ${description},
+        Tech Stack: ${techStackNames},
+        Features: ${featuresList}       
+    `,
+    BuildStepListSchema
+  );
+
+  return parsed;
+}

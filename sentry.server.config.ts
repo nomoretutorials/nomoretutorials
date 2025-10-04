9688f9bac332963268db88660cc97b9b6a8b29a4
Ronit Kedia <kediaronit.21@gmail.com>
2025-10-05 04:00:49 +0530

    feat(SentryIntegration): add Sentry for error tracking and monitoring across client, server, and edge functions

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://72e4381546539fefe09103ee5762de8a@o4510133544288256.ingest.us.sentry.io/4510133552283648",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});

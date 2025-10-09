88678f5ffcd8f52f669ed344a459ac293cc10066
Ronit Kedia <kediaronit.21@gmail.com>
2025-10-09 17:10:36 +0530

    refactor(inngest): remove inngest functions for agent running

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,14 +1,8 @@
-import { generateBuildStepsJob } from "@/inngest/functions/generate-build-steps-job";
-import { generateFeatureJob } from "@/inngest/functions/generate-features-job";
 import { serve } from "inngest/next";
 
-
-
 import { inngest } from "../../../inngest/client";
-import { generateBuildStepContentJob } from "@/inngest/functions/generate-step-content-job";
-
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [generateFeatureJob, generateBuildStepsJob, generateBuildStepContentJob],
+  functions: [],
 });

8b72eff9fe07404dc29cfbd961cdf42bb86e0f46
Ronit Kedia <kediaronit.21@gmail.com>
2025-11-13 15:14:53 +0530

    refactor(ReactQueryProvider): simplify query client management and persister initialization

--- src/app/ReactQueryProvider.tsx
+++ src/app/ReactQueryProvider.tsx
@@ -1,4 +1,4 @@
-// app/ReactQueryProvider.tsx   (or providers/ReactQueryProvider.tsx)
+// app/ReactQueryProvider.tsx
 "use client";
 
 import { ReactNode } from "react";
@@ -11,30 +11,18 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        // With SSR, we usually want to set some default staleTime
-        // above 0 to avoid refetching immediately on the client
         staleTime: 60 * 1000,
       },
     },
   });
 }
 
-const persister =
-  typeof window !== "undefined"
-    ? createAsyncStoragePersister({ storage: window.localStorage })
-    : undefined;
-
 let browserQueryClient: QueryClient | undefined = undefined;
 
 function getQueryClient() {
   if (isServer) {
-    // Server: always make a new query client
     return makeQueryClient();
   } else {
-    // Browser: make a new query client if we don't already have one
-    // This is very important, so we don't re-make a new client if React
-    // suspends during the initial render. This may not be needed if we
-    // have a suspense boundary BELOW the creation of the query client
     if (!browserQueryClient) browserQueryClient = makeQueryClient();
     return browserQueryClient;
   }
@@ -43,9 +31,10 @@ function getQueryClient() {
 export function ReactQueryProvider({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
 
-  if (isServer) {
-    return children;
-  }
+  // Create persister only on client side
+  const persister = isServer
+    ? undefined
+    : createAsyncStoragePersister({ storage: window.localStorage });
 
   return (
     <PersistQueryClientProvider
@@ -53,8 +42,6 @@ export function ReactQueryProvider({ children }: { children: ReactNode }) {
       persistOptions={{ persister: persister!, maxAge: Infinity }}
     >
       {children}
-
-      {/* DevTools only in development */}
       {process.env.NODE_ENV === "development" && <ReactQueryDevtools initialIsOpen={false} />}
     </PersistQueryClientProvider>
   );

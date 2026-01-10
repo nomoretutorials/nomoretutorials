888638d8528b7174a7c335b30ca048596e454865
Ronit Kedia <kediaronit.21@gmail.com>
2026-01-10 19:25:52 +0530

    feat: enhance metadata with Open Graph and Twitter card details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,26 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "NoMoreTutorials",
   description: "Stop Watching. Start Building.",
+  openGraph: {
+    title: "NoMoreTutorials",
+    description:
+      "An AI-powered platform that guides beginner and intermediate developers out of tutorial-hell by providing personalized, mentor-style step-by-step instructions to build real, production-grade projects from scratch.",
+    images: [
+      {
+        url: "https://app.nomoretutorials.com/dashboard.png",
+        width: "1200",
+        height: "630",
+        alt: "NoMoreTutorials Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "NoMoreTutorials",
+    description:
+      "An AI-powered platform that guides beginner and intermediate developers out of tutorial-hell by providing personalized, mentor-style step-by-step instructions to build real, production-grade projects from scratch.",
+    images: ["https://app.nomoretutorials.com/dashboard.png"],
+  },
 };
 
 export default function RootLayout({
